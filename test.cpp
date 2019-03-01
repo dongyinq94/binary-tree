b99de08c8ca27edb283a22ef6c4df3313beb23dd
dongyinq94 <247341387@qq.com>
2019-03-01 21:46:59 +0800

    first commit

--- test.cpp
+++ test.cpp
@@ -10,13 +10,16 @@ struct student
 
 int main(int argc, char const *argv[])
 {
-    student a,b;
+    student a,b,c;
     a.num=1;
     a.score=1;
     b.num=2;
     b.score=1;
     a.next=&b;
     b.next= NULL;
+    c.num=1;
+    c.score=3;
+    c.next=NULL;
     cout<<a.next->num<<endl;
     return 0;
 }
